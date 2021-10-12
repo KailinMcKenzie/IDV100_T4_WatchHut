e9059c28f8c2c79c7965220d2fd07c60417abf74
LourensSmith0602 <91066752+LourensSmith0602@users.noreply.github.com>
2021-10-12 15:37:41 +0200

    Api and js working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-//API implimentation link 'https://api.themoviedb.org/3/movie/550?api_key=c990b7c8aed9de2bea47d85625956c24';
+$(function() {
+    const url = 'https://api.themoviedb.org/3/movie/550?api_key=c990b7c8aed9de2bea47d85625956c24';
+
+    $.getJSON(url, function(result) {
+        console.log(result);
+
+    });
+
+});
 
 
 
