0f33e10b8404e5dc0cc64e34bf3c79045c591586
LourensSmith0602 <91066752+LourensSmith0602@users.noreply.github.com>
2021-10-14 16:25:19 +0200

    started adding content blocks slider needs work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+
+
+var sliderNav = function(manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active")
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    });
+
+    
+
+    btns[manual].classList.add("active")
+    slides[manual].classList.add("active")
+
+}
+
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+        
+});
+
+
 $(function() {
+
+
 
     const url = 'https://api.themoviedb.org/3/discover/movie?api_key=c990b7c8aed9de2bea47d85625956c24&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1';
 
@@ -24,8 +53,4 @@ $(function() {
 
     });
 
-});
-
-$(function() {
-    console.log("working");
 });
