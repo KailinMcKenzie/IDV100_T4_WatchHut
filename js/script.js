073a0f3c9c333cabb25c045d317e368e18db0889
LourensSmith0602 <91066752+LourensSmith0602@users.noreply.github.com>
2021-10-14 08:54:16 +0200

    new api with more movies

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 $(function() {
-    const url = 'https://api.themoviedb.org/3/movie/550?api_key=c990b7c8aed9de2bea47d85625956c24';
+
+    const url = 'https://api.themoviedb.org/3/discover/movie?api_key=c990b7c8aed9de2bea47d85625956c24&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1';
 
     $.getJSON(url, function(result) {
         console.log(result);
+
+        for(i=0; i < result.length; i++ ){
+            console.log(result[i].name);
+
+
+            
+
+        }
+        
 
     });
 
