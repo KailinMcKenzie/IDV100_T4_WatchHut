56ba8555b6f5969f09756f502fd8804071349d22
LourensSmith0602 <91066752+LourensSmith0602@users.noreply.github.com>
2021-10-16 08:19:18 +0200

    finished homepage and started with individual movies

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//Video Slider
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".vieo-slide");
 
 
 var sliderNav = function(manual) {
@@ -30,6 +33,7 @@ $(function() {
 
 
     const url = 'https://api.themoviedb.org/3/discover/movie?api_key=c990b7c8aed9de2bea47d85625956c24&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1';
+    const url = 'https://api.themoviedb.org/3/movie/latest?api_key=c990b7c8aed9de2bea47d85625956c24&language=en-US'
 
     $.getJSON(url, function(result) {
         console.log(result);
