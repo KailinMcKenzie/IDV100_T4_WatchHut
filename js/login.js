c779488911b45e8590793bd7315aa54ee53b5203
KailinMcKenzie <90200839+KailinMcKenzie@users.noreply.github.com>
2021-10-18 21:31:46 +0200

    Authentication and Redirection
    Authentication working, problem solving the Redirection.

--- js/login.js
+++ js/login.js
@@ -7,13 +7,23 @@
       $(".button-1").hide()
       $(".reg-container").slideDown();
   });
+// error function
+ $(".log-btn").click(function(){
 
+    if($(".required").val().length === 0){
+        alert("please fill in required fields")
+        $(".indicate").css("backgroundColor", "red" ); 
+    }
+ });
+// error function
+
+//Authentication function for login page
  $(".log-btn").on("click", function(){
 
     var username = $(".username").val();
     var password = $(".password").val();
 
-     url = "https://owmakerspace.co.za/users/data.json";
+    const url = "https://owmakerspace.co.za/users/data.json";
 
     $.getJSON(url, function(result){
         console.log(result.users);
@@ -28,16 +38,16 @@
                         window.location.href = "../index.html";
                         sessionStorage.setItem("username", username);
                     }else {
-                       // console.log("Account not active, please contact support.")
+                        // alert("Account not active, please contact support")
                         $(".indicate").css("backgroundColor", "red" );                     }
                 }else {
-                   // alert("please fill in required fields")
+                    // alert("please fill in required fields")
                     $(".indicate").css("backgroundColor", "red" ); 
 
                 }
 
             }else {
-               // alert("please fill in required fields")
+                // alert("please fill in required fields")
                 $(".indicate").css("backgroundColor", "red" ); 
             }
         }
@@ -47,4 +57,28 @@
 
 
  });
+
+ // Sign in function of redirecting
+ $('.sign').click(function(){
+    // alert("click")
+    var firstName= $('#name1').val();
+    var surname = $('#surname1').val();
+    var username = $('#newUser1').val();
+
+    sessionStorage.setItem('firstname', firstName);
+    console.log(firstName);
+
+    sessionStorage.setItem('surname', surname);
+    console.log(surname);
+
+    sessionStorage.setItem('username', username);
+
+    
+
+
+
+ });
+
+
+
 });
