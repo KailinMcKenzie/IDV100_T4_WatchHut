154206c94e651c8a1c3013a30820c4c3d7e62f0e
KailinMcKenzie <90200839+KailinMcKenzie@users.noreply.github.com>
2021-10-18 18:03:23 +0200

    Busy implementing authentication
    Coding authentication, testing live server

--- js/login.js
+++ js/login.js
@@ -8,12 +8,43 @@
       $(".reg-container").slideDown();
   });
 
- $(".log-btn").click(function(){
+ $(".log-btn").on("click", function(){
 
-    if($(".required").val().length === 0){
-        alert("please fill in required fields")
-        $(".indicate").css("backgroundColor", "red" ); 
-    }
- });
+    var username = $(".username").val();
+    var password = $(".password").val();
+
+     url = "https://owmakerspace.co.za/users/data.json";
+
+    $.getJSON(url, function(result){
+        console.log(result.users);
+
+        for(i = 0; i < result.users.length; i++){
+
+            if(result.users[i].username === username){
+
+                if(result.users[i].password === password){
+
+                    if(result.users[i].account === "active"){
+                        window.location.href = "../index.html";
+                        sessionStorage.setItem("username", username);
+                    }else {
+                       // console.log("Account not active, please contact support.")
+                        $(".indicate").css("backgroundColor", "red" );                     }
+                }else {
+                   // alert("please fill in required fields")
+                    $(".indicate").css("backgroundColor", "red" ); 
+
+                }
+
+            }else {
+               // alert("please fill in required fields")
+                $(".indicate").css("backgroundColor", "red" ); 
+            }
+        }
+
+    });
+
+
 
  });
+});
