c4a697b68c258e7ac1386cd7052cca5e22b8b175
anchenayres <21100284@virtualwindow.co.za>
2021-10-27 15:32:49 +0200

    Genre
    Filter working

--- js/library.js
+++ js/library.js
@@ -14,8 +14,7 @@ $(function() {
 
             
 
-            var MovieBlock = "<div class='col-lg-2 col-md-4 col-sm-4 block'>\
-            <div class='one'>\
+            var MovieBlock = "<div class='one one"+ Movies.results[i].genre_ids[0] +" one"+ Movies.results[i].genre_ids[1] +" col-lg-2 col-md-4 col-sm-4 block'>\
               <div class='image'><img src='https://image.tmdb.org/t/p/original"+ Movies.results[i].poster_path +"'style='height: 100%; width: 100%; object-fit: cover'><div class='knoppie'>Watch Later</div><div class='Save'></div>\
               <div class='info'>\
                 <h3 class='Movie_title'>"+ Movies.results[i].original_title +"</h3>\
@@ -24,12 +23,11 @@ $(function() {
                   <li class='Genre'>Genre</li>\
                   <li class='Pg'>Adult</li>\
                 </ul>\
-              </div>\
             </div><!-- one -->\
           </div>"
             
 
-            $('.row').append(MovieBlock);
+            $('.movies-row').append(MovieBlock);
 
             var adult = Movies.results[i].adult;
 
@@ -51,9 +49,72 @@ $(function() {
 
       console.log(Genre);
 
+      
      
  
     });
+
+
+    //filter
+    $(".all").click(function() {
+      //first remove all active states
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".all").addClass("filter-active");
+      
+      $(".one").fadeIn();
+  });
+
+  $(".horror").click(function() {
+
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".horror").addClass("filter-active");
+
+      $(".one").hide();
+      $(".one27").fadeIn();
+
+  });
+
+  $(".animation").click(function() {
+      //first remove all active states
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".animation").addClass("filter-active");
+
+      $(".one").hide();
+      $(".one16").fadeIn();
+  });
+
+  $(".comedy").click(function() {
+      //first remove all active states
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".comedy").addClass("filter-active");
+
+      $(".one").hide();
+      $(".one35").fadeIn();
+  });
+
+  $(".romance").click(function() {
+      //first remove all active states
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".romance").addClass("filter-active");
+
+      $(".one").hide();
+      $(".one10749").fadeIn();
+  });
+
+  $(".fiction").click(function() {
+      //first remove all active states
+      $(".filter-button").removeClass("filter-active");
+      //adding a active state
+      $(".fiction").addClass("filter-active");
+
+      $(".one").hide();
+      $(".one878").fadeIn();
+  });
 
     
      
