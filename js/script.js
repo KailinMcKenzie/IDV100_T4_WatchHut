fbe7880434dec04821e4cbfe4483e97b980b5174
LourensSmith0602 <91066752+LourensSmith0602@users.noreply.github.com>
2021-10-29 11:47:27 +0200

    finished movies page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggle(){
+    var trailer=document.querySelector('.trailer');
+    let video=document.querySelector('.video');
+    trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
+}
